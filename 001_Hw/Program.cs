d74f706e7089a41512dc1e6000addb240343388f
Nadin <nmsmet@ya.ru>
2022-07-19 14:52:01 +0300

    Add folder 001_Hw

---
+++ 001_Hw/Program.cs
@@ -0,0 +1,19 @@
+﻿int n;
+Console.WriteLine("Напишите количество чисел: ");
+n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+        count++;
+}
+Console.WriteLine(count);
