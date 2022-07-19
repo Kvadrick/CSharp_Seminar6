55cc6cad9b90b7bbdaaedde05357e89f8228d47d
Nadin <nmsmet@ya.ru>
2022-07-19 14:52:24 +0300

    Add folder 002_Hw

---
+++ 002_Hw/Program.cs
@@ -0,0 +1,22 @@
+﻿double b1;
+Console.WriteLine("Введите b1: ");
+b1 = Convert.ToInt32(Console.ReadLine());
+
+double k1;
+Console.WriteLine("Введите k1: ");
+k1 = Convert.ToInt32(Console.ReadLine());
+
+double b2;
+Console.WriteLine("Введите b2: ");
+b2 = Convert.ToInt32(Console.ReadLine());
+
+double k2;
+Console.WriteLine("Введите k2: ");
+k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"({x}; {y})");
+
+
